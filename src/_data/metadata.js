f4fc55947fc0885e7841088b1ad3d4d467107c1a
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-09-11 22:57:08 -0500

    chore: test this

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,4 +2,10 @@ export default {
 	title: process.env.TITLE,
 	lang: process.env.LANGUAGE || "en",
 	locale: process.env.LOCALE || "en_US",
+
+
+	repository: process.env.REPOSITORY_URL,
+	branch: process.env.BRANCH,
+	head: process.env.BRANCH,
+	envs: JSON.stringify(process.env),
 }
