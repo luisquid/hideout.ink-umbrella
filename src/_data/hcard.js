f6d1c8d2094c73842d0ce99e0b116c2f4266e8c6
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-09-11 23:29:22 -0500

    feat: using .env

--- src/_data/hcard.js
+++ src/_data/hcard.js
@@ -1,5 +1,5 @@
 export default {
-	url: process.env.ME || process.env.URL,
+	url: process.env.URL,
 	name: process.env.NAME,
 	email: process.env.EMAIL,
 	note: process.env.NOTE,
--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,10 +2,4 @@ export default {
 	title: process.env.TITLE,
 	lang: process.env.LANGUAGE || "en",
 	locale: process.env.LOCALE || "en_US",
-
-
-	repository: process.env.REPOSITORY_URL,
-	branch: process.env.BRANCH,
-	head: process.env.BRANCH,
-	envs: JSON.stringify(process.env),
 }
