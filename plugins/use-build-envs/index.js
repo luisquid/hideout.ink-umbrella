1d56524a1896cc2f83824b79dd3cc9cee3f29ebf
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-09-16 00:39:08 -0500

    wip: typo

--- plugins/use-build-envs/index.js
+++ plugins/use-build-envs/index.js
@@ -1,3 +1,6 @@
 export const onBuild = ({ netlifyConfig }) => {
-	netlifyConfig.functions.environment.REPOSITORY_URL = process.env.REPOSITORY_URL
+	console.log('onBuild', process.env.REPOSITORY_URL)
+	console.log('- ', JSON.stringify(netlifyConfig.functions))
+	if (!netlifyConfig.functions.env) netlifyConfig.functions.env = {}
+	netlifyConfig.functions.env.REPO_URL = process.env.REPOSITORY_URL
 }
