69b551f1c6f762af06abd0dbd524443a1282886a
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-09-16 10:25:06 -0500

    feat: nevermind. REPOSITORY_URL and BRANCH won't work

--- netlify/mp-config.js
+++ netlify/mp-config.js
@@ -2,13 +2,17 @@ import Micropub from '@benjifs/micropub'
 import GitHubStore from '@benjifs/github-store'
 const {
 	URL,
-	REPOSITORY_URL,
-	BRANCH,
+	// REPOSITORY_URL,
+	// BRANCH,
 	GITHUB_USER,
 	GITHUB_REPO,
 	GITHUB_BRANCH,
 	GITHUB_TOKEN,
 } = process.env
+/*
+ * I can't figure out a good way to use the Netlify "build" Environment Variables
+ * so just leave this out for now.
+ * https://docs.netlify.com/build/functions/environment-variables/#overrides-and-limitations
 let username, repo
 // https://docs.netlify.com/build/configure-builds/environment-variables/#git-metadata
 // Assume github for this example
@@ -19,18 +23,15 @@ if (!GITHUB_USER && !GITHUB_REPO && REPOSITORY_URL?.indexOf('github.com')) {
 		repo = parts.at(-2)
 	}
 }
+*/
 // @benjifs/indieauth normalizes URL so this should too here
 const normalizeMe = (URL || '').replace(/\/+$/, '') + '/'
-console.log(`URL: ${URL}`)
-console.log(`normalized: ${normalizeMe}`)
-console.log(`Expected: ${REPOSITORY_URL} - ${username} - ${repo} - ${BRANCH}`)
-console.log(`Provided: ${GITHUB_USER} - ${GITHUB_REPO} -${GITHUB_BRANCH}`)
 export const micropub = new Micropub({
 	store: new GitHubStore({
 		token: GITHUB_TOKEN,
-		user: username || GITHUB_USER,
-		repo: repo || GITHUB_REPO,
-		branch: BRANCH || GITHUB_BRANCH,
+		user: GITHUB_USER,
+		repo: GITHUB_REPO,
+		branch: GITHUB_BRANCH,
 	}),
 	me: normalizeMe,
 	tokenEndpoint: `${URL}/token`,
