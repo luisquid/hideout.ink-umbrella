1879acf42454750984d5594051cb901cf0845608
Benji Encalada Mora <benjifs@users.noreply.github.com>
2025-09-16 00:47:54 -0500

    wip: last plugin test

--- plugins/use-build-envs/index.js
+++
@@ -1,8 +0,0 @@
-export const onBuild = ({ netlifyConfig }) => {
-	console.log('onBuild', process.env.REPOSITORY_URL)
-	console.log('- ', JSON.stringify(netlifyConfig.functions))
-	if (!netlifyConfig.functions.env) netlifyConfig.functions.env = {}
-	netlifyConfig.functions.env.REPOSITORY_URL = process.env.REPOSITORY_URL
-	if (!netlifyConfig.functions.environment) netlifyConfig.functions.environment = {}
-	netlifyConfig.functions.environment.REPOSITORY_URL = process.env.REPOSITORY_URL
-}
